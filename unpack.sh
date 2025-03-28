e790b11de9be59c9a1a075bdb7bdd10b558f6130
aymangosh <aymansce@gmail.com>
2025-03-28 19:54:17 +0300

    write v1 of dfs algorithm

--- unpack.sh
+++ unpack.sh
@@ -1,7 +1,23 @@
-#!/bin/bash 
+#!/bin/bash
 
+# DFS function to explore directories
+dfs() {
+    local dir="$1"
 
-echo "test"
+    # Loop through all files and directories in the current directory
+    for file in "$dir"/*; do
+        # Check if it is a directory
+        if [ -d "$file" ]; then
+            echo "Directory: $file"
+            # Recursively call dfs on the subdirectory
+            dfs "$file"
+        elif [ -f "$file" ]; then
+            # If it's a file, just print it
+            echo "File: $file"
+        fi
+    done
+}
 
-
-
+# Start DFS from the current directory
+start_dir=$(pwd)  # Get the current directory
+dfs "$start_dir"
