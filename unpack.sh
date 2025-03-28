f1204300677e47b7c21af0896b2c1b84006aecd9
aymangosh <aymansce@gmail.com>
2025-03-28 21:48:38 +0300

    clean the code

--- unpack.sh
+++ unpack.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 # Function to handle archives and unpack them if necessary
 handle_archive() {
     local file="$1"
     local temp_dir="$2"
-
     echo "Unpacking $file..."
-    
     case "$file" in
         *.tar.gz|*.tgz)
             tar -xzf "$file" -C "$temp_dir" || { echo "Failed to unpack $file"; return 1; }
@@ -25,13 +22,10 @@ handle_archive() {
             return 1
             ;;
     esac
-
 }
-
 # DFS function to explore directories
 dfs() {
     local dir="$1"
-    
     # Loop through all files and directories in the current directory
     for file in "$dir"/*; do
         if [ -d "$file" ]; then
@@ -42,25 +36,19 @@ dfs() {
             case "$file" in
                 *.tar.gz|*.tgz|*.tar.bz2|*.tbz|*.tar|*.zip)
                     # Create a temporary directory to unpack the archive into
-
                     handle_archive "$file" "$dir"
                     base_path=${file##*/}	
                     file_name1=${base_path%.*.*}
-
                     # After unpacking, call dfs again on the extracted directory
                     if [ -d "$dir"/"$file_name1" ]; then
                         echo "Entering extracted directory: $file"
                         dfs "$dir"/"$file_name1"
                     fi
-                    
                     ;;
-
-
             esac
         fi
     done
 }
-
 # Start DFS from the current directory
 start_dir=$(pwd)  # Get the current directory
 dfs "$start_dir"
