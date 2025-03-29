003a753a5fe35ccd4abf897e13411e7bddbe0bf6
aymangosh <aymansce@gmail.com>
2025-03-29 13:25:37 +0300

    cleaning

--- unpack.sh
+++ unpack.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Default values
 recursive=false
 verbose=false
@@ -13,36 +12,25 @@ while getopts "rv" opt; do
            exit 1 ;;
     esac
 done
-
 # Remove parsed options from arguments
 shift $((OPTIND - 1))
-
 # Ensure at least one file is provided
 if [ $# -lt 1 ]; then
     echo "Error: At least one file must be specified."
     echo "Usage: $0 [-r] [-v] file [files...]"
     exit 1
 fi
-
-
 handle_archive() {
     local file="$1"
     local temp_dir="$2"
-
     base_name=$(basename "$file")  # Get filename with extension
     file_name="${base_name%.*}"    # Remove only last extension (keeps base name)
-
     # Create a unique extraction path based on both base name and file extension
     extract_dir="$temp_dir/${file_name}_extracted_${base_name##*.}"
     mkdir -p "$extract_dir"
-
     if [ "$verbose" = true ]; then
         echo "Unpacking" $file_name
     fi
-    
-
-
-
     case "$file" in
         *.tar.gz|*.tgz)
             tar -xzf "$file" -C "$extract_dir" || { echo "Failed to unpack $file";}
@@ -69,23 +57,14 @@ handle_archive() {
             ((counter+=1)) 
             ;;
     esac
-
     # Immediately scan the extracted directory for more archives
- 
     if [ "$recursive" = true ]; then
          dfs "$extract_dir"
     fi
-   
 }
-
-
 # DFS function to explore directories
 dfs() {
     local dir="$1"
-    
-    # if -v
-    #echo "Scanning directory: $dir"
-
     # Loop through all files and directories in the current directory
     for file in "$dir"/*; do
         if [ -d "$file" ]; then
@@ -95,7 +74,6 @@ dfs() {
             # If it's an archive, unpack it
             case "$file" in
                 *.tar.gz|*.tgz|*.tar.bz2|*.tbz|*.tar|*.zip|*.gz|*.bz2|*.Z)
-                    
                     handle_archive "$file" "$dir"
                     ;;
                 *)
@@ -105,13 +83,11 @@ dfs() {
                         file_name="${base_name%.*}"    # Remove only last extension (keeps base name)
                         echo "Ignoring " $file_name
                     fi
-                    ;;
-                    
+                    ;; 
             esac
         fi
     done
 }
-
 for file in "$@"; do
     if [ -f "$file" ]; then
         # Extract the archive
@@ -120,7 +96,6 @@ for file in "$@"; do
                 handle_archive "$file" "$(pwd)"
                 ;;
             *)
-                #echo "Unsupported archive type: $file"
                 if [ "$verbose" = true ]; then
                     base_name=$(basename "$file")  # Get filename with extension
                     file_name="${base_name%.*}"    # Remove only last extension (keeps base name)
@@ -128,11 +103,6 @@ for file in "$@"; do
                 fi
                 ;;
         esac
-    #else
-        #echo "File not found: $file"
     fi
-    
 done
-
-
 echo "Decompressed $counter archive(s)"
